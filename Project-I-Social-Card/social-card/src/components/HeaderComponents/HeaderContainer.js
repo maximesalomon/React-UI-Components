995ba83134ad023c827929e30ee415f538056517
Maxime Salomon <maxime@croissant.io>
2019-01-16 19:58:31 +0100

    first react app

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+      <div class="header-container">
+        <div class="image-thumbnail"><ImageThumbnail /></div>
+        <div class="image-all">
+          <div class="image-title"><HeaderTitle /></div>
+          <div class="image-content"><HeaderContent /></div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default HeaderContainer;
