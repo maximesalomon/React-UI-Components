b9a581c53565a993c35888ec834249dcc1613df3
Maxime Salomon <maxime@croissant.io>
2019-02-11 16:25:39 +0100

    social card + calc MVP done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 
 const CardContainer = () => {
     return (
       <div className="card-container">
         <div><CardBanner/></div>
-        <div className="card-content">
-          <h4>Get started with React</h4>
-          <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-          <p className="bannerLink">reactjs.org</p>
-        </div>
+        <div className="card-content"><CardContent /></div>
       </div>
     );
   };
