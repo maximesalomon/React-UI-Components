96db2a8c1fd8066fd78c37e1e9c8f421db4ba027
Maxime Salomon <maxime@croissant.io>
2019-02-11 15:18:52 +0100

    added instagram card MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,7 +6,12 @@ import CardBanner from './CardBanner';
 const CardContainer = () => {
     return (
       <div className="card-container">
-      <CardBanner/>
+        <div><CardBanner/></div>
+        <div className="card-content">
+          <h4>Get started with React</h4>
+          <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+          <p className="bannerLink">reactjs.org</p>
+        </div>
       </div>
     );
   };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Header.css';
 
+const now = Date.now();
+
 const HeaderTitle = () => {
     return (
-        <p>Lambda School  @LambdaSchool - 26 jan</p>
+        <div className="header-title">
+            <h3>Lambda School</h3>
+            <p>@LambdaSchool</p>
+            <p>February 11th, 2019</p>
+        </div>
     );
   };
   
